e63276f17964468e58893cbcd9cd6e5217cf0085
Claire Chen <claire13985@gmail.com>
2023-08-19 00:55:24 -0400

    Updated logos and text

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,10 +2,10 @@ import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
 import projImg1 from "../assets/img/ourchoice.png";
 import projImg2 from "../assets/img/foodzoo.png";
-import projImg3 from "../assets/img/project-img3.png";
-import projImg4 from "../assets/img/ourchoice.png";
-import projImg5 from "../assets/img/foodzoo.png";
-import projImg6 from "../assets/img/project-img3.png";
+import projImg3 from "../assets/img/froggyducky.png";
+import projImg4 from "../assets/img/playte.png";
+import projImg5 from "../assets/img/mm.png";
+import projImg6 from "../assets/img/ibobby.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
@@ -20,28 +20,28 @@ export const Projects = () => {
     },
     {
       title: "The Food Zoo",
-      description: "Design & Development",
+      description: "HTML, CSS, JavaScript",
       imgUrl: projImg2,
     },
     {
       title: "Froggy & Ducky",
-      description: "Design & Development",
+      description: "HTML, JavaScript, GSAP, Tiled",
       imgUrl: projImg3,
     },
     {
       title: "Playte",
-      description: "Design & Development",
-      imgUrl: projImg1,
+      description: "Swift, XCode, CreateML",
+      imgUrl: projImg4,
     },
     {
       title: "Maryland Munch",
-      description: "Design & Development",
-      imgUrl: projImg2,
+      description: "Thunkable, Figma",
+      imgUrl: projImg5,
     },
     {
       title: "IB Obby",
-      description: "Design & Development",
-      imgUrl: projImg3,
+      description: "Lua, Roblox Studio",
+      imgUrl: projImg6,
     },
   ];
 
@@ -86,10 +86,59 @@ export const Projects = () => {
                       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
                     </Tab.Pane>
                     <Tab.Pane eventKey="second">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                      <div class="description">
+                        <h2>
+                          OurChoice
+                        </h2>
+                        <ul>
+                          <li>OurChoice is an educational website on the history of abortion and its restrictions in the US. </li>
+                        </ul>
+                      </div>
+                      <div class="description">
+                        <h2>
+                          The Food Zoo
+                        </h2>
+                        <ul>
+                          <li>The Food Zoo is an interactive website that teaches children the basics of nutrition based on a quiz that retrieves information about their current diet. </li>
+                        </ul>
+                      </div>
+                      <div class="description">
+                        <h2>
+                          Froggy & Ducky
+                        </h2>
+                        <ul>
+                          <li>Froggy & Ducky is a Fireboy & Watergirl inspired game where the Froggy, controlled by WASD keys, and the Ducky, led by arrow keys, have to maneuver past dangerous crystals to reach their teleportation platform and advance to the next level. </li>
+                        </ul>
+                      </div>
+                      <div class="description">
+                        <h2>
+                          Playte
+                        </h2>
+                        <ul>
+                          <li>Playte is an iOS app that uses a machine learning model trained on a dataset of over 750+ images to classify a user's inputted food and ingredients. The model will predict what recipes are possible based on the given images, allowing the user to reduce waste and create a delicious meal from existing materials.</li>
+                        </ul>
+                      </div>
+                      <div class="description">
+                        <h2>
+                          Maryland Munch
+                        </h2>
+                        <ul>
+                          <li>Maryland Munch is a mobile budgeting app designed for students and faculty at UMD. The app features a budgeting system that tracks a user's expenses, a map that locates nearby restaurants and dining halls, and a trending page with student reviews and popular, affordable meals. </li>
+                        </ul>
+                      </div>
+                      <div class="description">
+                        <h2>
+                          IB Obby
+                        </h2>
+                        <ul>
+                          <li>IB Obby is a Roblox obstacle course game created for students in the International Baccalaureate Diploma Programme. The game uses different styles of obstacles, including trivia on the IB Program, to test players. </li>
+                        </ul>
+                      </div>
                     </Tab.Pane>
                     <Tab.Pane eventKey="third">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                        <h2>
+                          Garuda Hacks 2022, 1st Runner Up for Best All Girls Hack
+                        </h2>
                     </Tab.Pane>
                   </Tab.Content>
                 </Tab.Container>
