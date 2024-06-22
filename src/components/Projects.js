a25d0164d01b2321c1d1484857c4ee63d61f8cfd
Claire <claire13985@gmail.com>
2024-06-21 20:47:00 -0400

    Added new projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,6 +6,10 @@ import projImg3 from "../assets/img/froggyducky.png";
 import projImg4 from "../assets/img/playte.png";
 import projImg5 from "../assets/img/mm.png";
 import projImg6 from "../assets/img/ibobby.png";
+import projImg7 from "../assets/img/forecasts.png";
+import projImg8 from "../assets/img/studyterp.png";
+import projImg9 from "../assets/img/coffeespacecat.png";
+import projImg10 from "../assets/img/tom.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
@@ -13,6 +17,26 @@ import TrackVisibility from 'react-on-screen';
 export const Projects = () => {
 
   const projects = [
+    {
+      title: "Coffee Space Cats",
+      description: "JavaScript",
+      imgUrl: projImg9,
+    },
+    {
+      title: "T.O.M.",
+      description: "Python, Django, HTML, CSS, JavaScript",
+      imgUrl: projImg10,
+    },
+    {
+      title: "Forecasts by the Campfire",
+      description: "pandas, Jupyter, Figma",
+      imgUrl: projImg7,
+    },
+    {
+      title: "Study Terp",
+      description: "HTML, CSS, JavaScript",
+      imgUrl: projImg8,
+    },
     {
       title: "OurChoice",
       description: "HTML, CSS, JavaScript",
@@ -61,10 +85,10 @@ export const Projects = () => {
                       <Nav.Link eventKey="first">Visuals</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                      <Nav.Link eventKey="second">About</Nav.Link>
+                      <Nav.Link eventKey="second">Descriptions</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                      <Nav.Link eventKey="third">Resume</Nav.Link>
+                      <Nav.Link eventKey="third">Awards & Activities</Nav.Link>
                     </Nav.Item>
                   </Nav>
                   <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
@@ -83,60 +107,90 @@ export const Projects = () => {
                       </Row>
                     </Tab.Pane>
                     <Tab.Pane eventKey="section">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                      <p></p>
                     </Tab.Pane>
                     <Tab.Pane eventKey="second">
+                    <div class="description">
+                        <h2>Coffee Space Cats</h2>
+                        <ul>
+                          <li>This VS Code extension decorates your IDE with a cool, modern theme. It's all about coffee cats in space! </li>
+                        </ul>
+                      </div>
+                    <div class="description">
+                        <h2>Tabby of Management (T.O.M.)</h2>
+                        <ul>
+                          <li>T.O.M. is a talking cat that uses Google's Gemini Arificial Intelligence API and a text-to-speech feature to generate 
+                            personalized motivational quotes, daily schedules, and a study space for each user and reads it out to them. </li>
+                        </ul>
+                      </div>
                       <div class="description">
-                        <h2>
-                          OurChoice
-                        </h2>
+                        <h2>Forecasts By the Campfire</h2>
+                        <ul>
+                          <li>Get personalized housing and financial advice by sitting at the campfire with our cute creatures and discovering who you're most like! 
+                            This app takes in data surrounding the climate, frequency of natural disasters, and other geographical features affect housing preferences. 
+                            After extracting and analyzing data, an interactive quiz is generated and assigns each user a unique creature that best matches their descriptions 
+                            and provides them advice on next steps to take regarding their financial and homeowner decisions. </li>
+                        </ul>
+                      </div>
+                      <div class="description">
+                        <h2>Study Terp</h2>
+                        <ul>
+                          <li>Study Terp is a study buddy matchmaker for students at the University of Maryland. Through this revolutionary social study platform, 
+                            students are matched with study buddies based on classes, schedules, majors/minors, study habits, and overall compatibility. 
+                            Study Terp aims to foster a supportive and collaborative learning environment on demand for every student by connecting them with like-minded peers. 
+                            Say goodbye to solo study struggles and hello to a brighter, more interactive academic journey. Your perfect study buddy is just a click away!</li>
+                        </ul>
+                      </div>
+                      <div class="description">
+                        <h2>OurChoice</h2>
                         <ul>
                           <li>OurChoice is an educational website on the history of abortion and its restrictions in the US. </li>
                         </ul>
                       </div>
                       <div class="description">
-                        <h2>
-                          The Food Zoo
-                        </h2>
+                        <h2>The Food Zoo</h2>
                         <ul>
-                          <li>The Food Zoo is an interactive website that teaches children the basics of nutrition based on a quiz that retrieves information about their current diet. </li>
+                          <li>The Food Zoo is an interactive website that teaches children the basics of nutrition based on a quiz that retrieves 
+                            information about their current diet. </li>
                         </ul>
                       </div>
                       <div class="description">
-                        <h2>
-                          Froggy & Ducky
-                        </h2>
+                        <h2>Froggy & Ducky</h2>
                         <ul>
-                          <li>Froggy & Ducky is a Fireboy & Watergirl inspired game where the Froggy, controlled by WASD keys, and the Ducky, led by arrow keys, have to maneuver past dangerous crystals to reach their teleportation platform and advance to the next level. </li>
+                          <li>Froggy & Ducky is a Fireboy & Watergirl inspired game where the Froggy, controlled by WASD keys, and the Ducky, 
+                            led by arrow keys, have to maneuver past dangerous crystals to reach their teleportation platform and advance to the next level. </li>
                         </ul>
                       </div>
                       <div class="description">
-                        <h2>
-                          Playte
-                        </h2>
+                        <h2>Playte</h2>
                         <ul>
-                          <li>Playte is an iOS app that uses a machine learning model trained on a dataset of over 750+ images to classify a user's inputted food and ingredients. The model will predict what recipes are possible based on the given images, allowing the user to reduce waste and create a delicious meal from existing materials.</li>
+                          <li>Playte is an iOS app that uses a machine learning model trained on a dataset of over 750+ images to classify a 
+                            user's inputted food and ingredients. The model will predict what recipes are possible based on the given images, 
+                            allowing the user to reduce waste and create a delicious meal from existing materials.</li>
                         </ul>
                       </div>
                       <div class="description">
-                        <h2>
-                          Maryland Munch
-                        </h2>
+                        <h2>Maryland Munch</h2>
                         <ul>
-                          <li>Maryland Munch is a mobile budgeting app designed for students and faculty at UMD. The app features a budgeting system that tracks a user's expenses, a map that locates nearby restaurants and dining halls, and a trending page with student reviews and popular, affordable meals. </li>
+                          <li>Maryland Munch is a mobile budgeting app designed for students and faculty at UMD. The app features a budgeting system 
+                            that tracks a user's expenses, a map that locates nearby restaurants and dining halls, and a trending page with student 
+                            reviews and popular, affordable meals. </li>
                         </ul>
                       </div>
                       <div class="description">
-                        <h2>
-                          IB Obby
-                        </h2>
+                        <h2>IB Obby</h2>
                         <ul>
-                          <li>IB Obby is a Roblox obstacle course game created for students in the International Baccalaureate Diploma Programme. The game uses different styles of obstacles, including trivia on the IB Program, to test players. </li>
+                          <li>IB Obby is a Roblox obstacle course game created for students in the International Baccalaureate Diploma Programme. 
+                            The game uses different styles of obstacles, including trivia on the IB Program, to test players. </li>
                         </ul>
                       </div>
                     </Tab.Pane>
                     <Tab.Pane eventKey="third">
-                        <h2>Garuda Hacks 2022, 1st Runner Up for Best All Girls Hack</h2>
+                        <h2>Bitcamp 2024, Best DEI Hack</h2>
+                        <p>Forecasts by the Campfire</p>
+                        <h2>Technica 2023, Most Creative Use of Github</h2>
+                        <p>Study Terp</p>
+                        <h2>Garuda Hacks 2022, 2nd Best Best All Girls Hack</h2>
                         <p>The Food Zoo</p>
                         <h2>Technica Fellowship 2023</h2>
                         <p>Froggy & Ducky</p>
